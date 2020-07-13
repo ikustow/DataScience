6db89cad727d4577d01b43abade856956d90ceee
ikustow <ikustow@yandex.run>
2020-07-13 23:38:29 +0300

    commit

--- ds/models.py
+++ ds/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
 
@@ -16,7 +17,7 @@ class Purchase(models.Model):
     quantity = models.PositiveIntegerField()
     total_price = models.PositiveIntegerField(blank=True)
     salesman = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(default=timezone.now)
 
 
     def save(self,*args,**kwargs):
