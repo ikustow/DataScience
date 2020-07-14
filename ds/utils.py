7fd06cd855a992aee828c54da351c5fe7eb030ee
ikustow <ikustow@yandex.run>
2020-07-14 23:14:33 +0300

    commit

---
+++ ds/utils.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from io import BytesIO
+import base64
+
+def get_image():
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    image_png = buffer.getvalue()
+    graph = base64.b64encode(image_png)
+    graph = graph.decode('utf-8')
+    buffer.close()
+    return graph
+
+def get_simple_plot(chart_type, *args, **kwargs):
+
+    plt.switch_backend('AGG')
+    fig = plt.figure(figsize=(10,4))
+    x = kwargs.get('x')
+    y = kwargs.get('y')
+    data = kwargs.get('data')
+    if chart_type == 'bar plot':
+        title = "title"
+        plt.title(title)
+        plt.bar(x,y)
+    elif chart_type == 'line plot':
+        title = "title"
+        plt.title(title)
+        plt.plot(x,y)
+    else:
+        title = "title"
+        plt.title(title)
+        sns.countplot('name', data=data)
+    plt.tight_layout()
+    graph = get_image()
+    return graph
--- ds/views.py
+++ ds/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Product, Purchase
 import pandas as pd
+from .utils import get_simple_plot
 # Create your views here.
 
 def chart_select_view(request):
     error_message = None
     df = None
-
+    graph = None
     product_df = pd.DataFrame(Product.objects.all().values())
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
     product_df['product_id'] = product_df['id']
@@ -17,10 +18,24 @@ def chart_select_view(request):
             chart_type = request.POST['sales']
             date_from = request.POST['date_from']
             date_to = request.POST['date_to']
+
+            df['date'] = df['date'].apply(lambda x:x.strftime('%Y-%m-%d'))
+            df2=df.groupby('date', as_index=False)['total_price'].agg('sum')
+
+            if chart_type !="":
+                if date_from !="" and date_to != "":
+                    df = df[(df['date']>date_from) & (df['date']<date_to)]
+                    df2 = df.groupby ('date', as_index=False)['total_price'].agg ('sum')
+
+                graph = get_simple_plot(chart_type,x=df2['date'],y=df2['total_price'],data=df)
+            else:
+                error_message = "Please select a chart type"
+
     else:
         error_message = "No records in DataBase"
 
     context = {
         'error_message': error_message,
+        'graph': graph,
     }
     return render(request, 'ds/main.html', context)
